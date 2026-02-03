f7bb46b477a149d75eac4981f3444529ef5ecf8d
Dmytro-Doronin <doronindmytro623@gmail.com>
2026-02-03 10:02:10 +0100

    style: fonts and tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div>App</div>
     </>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import AppWrapper from "./AppWrapper";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <AppWrapper>
-    <App />
-  </AppWrapper>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
