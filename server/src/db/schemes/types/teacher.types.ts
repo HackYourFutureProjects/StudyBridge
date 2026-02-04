cdebafb43b949dae4fbb5f14abc4e1b30d35a97e
Alaa <angnm38@gmail.com>
2026-02-04 22:54:12 +0100

    Add teacher schema

---
+++ server/src/db/schemes/types/teacher.types.ts
@@ -0,0 +1,61 @@
+import { Role } from "../../../types/common.types.js";
+import { ObjectId } from "mongodb";
+
+type EducationItem = {
+  degree: string;
+  institution: string;
+};
+
+type SubjectItem = {
+  _id: ObjectId;
+  subjectName: string;
+  levels: string[];
+  experienceYears: number;
+  hourlyRate: number;
+};
+
+type TimeSlot = {
+  start: string;
+  end: string;
+};
+
+type AvailabilityItem = {
+  monday: TimeSlot[];
+  tuesday: TimeSlot[];
+  wednesday: TimeSlot[];
+  thursday: TimeSlot[];
+  friday: TimeSlot[];
+  saturday: TimeSlot[];
+  sunday: TimeSlot[];
+};
+
+export type AddressItem = {
+  street: string | null;
+  city: string | null;
+  state: string | null;
+  zipCode: string | null;
+  country: string | null;
+};
+
+export type TeacherTypeDB = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  passwordHash: string;
+  passwordSalt: string;
+  profileImageUrl: string | null;
+  experience: number;
+  bio: string | null;
+  headline: string | null;
+  phoneNumber: string | null;
+  dateOfBirth: Date | null;
+  gender: string | null;
+  mainLanguage: string | null;
+  education: EducationItem[];
+  subjects: SubjectItem[];
+  availability: AvailabilityItem;
+  address: AddressItem;
+  createdAt: Date;
+  role: Role;
+};
