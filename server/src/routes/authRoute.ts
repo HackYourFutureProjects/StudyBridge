a76155e70b800d63159b822bb12f48ef7ba5ba4b
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2026-02-04 16:50:54 +0100

    feat: error middleware

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { autStudentValidationMiddleware } from "../middlewares/auth/studentAuthMiddleware.js";
+import { autStudentValidationMiddleware } from "../validation/auth/studentAuthMiddleware.js";
+import { errorMiddleware } from "../middlewares/error.middleware.js";
 
 export const authRouter = Router();
 
 authRouter.post(
   "/registration-student",
   autStudentValidationMiddleware(),
+  errorMiddleware,
   // errorMiddleware,
   // authControllerInstance.registrationController.bind(authControllerInstance),
 );
