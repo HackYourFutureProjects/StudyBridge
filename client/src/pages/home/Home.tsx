48f2c0acbaf1ff536352eb5e25b693da52c63c30
Dasha <daryna2003tk@gmail.com>
2026-02-04 08:20:35 +0100

    add home page - features card

---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import { Features } from "../../components/home-section/Features/Features";
+
+export const Home = () => {
+  return (
+    <div className="h-auto">
+      <section className="section-spacing relative z-10 bg-bg-main">
+        <Features />
+      </section>
+    </div>
+  );
+};
