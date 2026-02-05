6dd911c87f29c61f4349f29e40e5792639646175
Alaa <angnm38@gmail.com>
2026-02-05 01:22:30 +0100

    register teacher dependencies in composition container

--- server/src/composition/composition.types.ts
+++ server/src/composition/composition.types.ts
@@ -2,5 +2,10 @@ export const TYPES = {
   StudentCommand: Symbol.for("StudentCommand"),
   StudentService: Symbol.for("StudentService"),
   StudentQuery: Symbol.for("StudentQuery"),
+
+  TeacherCommand: Symbol.for("TeacherCommand"),
+  TeacherService: Symbol.for("TeacherService"),
+  TeacherQuery: Symbol.for("TeacherQuery"),
+
   AuthController: Symbol.for("AuthController"),
 };
