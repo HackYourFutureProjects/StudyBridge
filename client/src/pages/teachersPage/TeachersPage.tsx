31503cbc0fe1f2f3d178520acadd0bad87785acd
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2026-02-10 14:36:31 +0100

    feat: teacher page

---
+++ client/src/pages/teachersPage/TeachersPage.tsx
@@ -0,0 +1,20 @@
+export const TeachersPage = () => {
+  return (
+    <div
+      className="
+            flex items-center
+            flex-col justify-center w-full max-w-360 mx-auto mt-20 py-20 px-20
+            "
+    >
+      <h1 className="auth-title">OUR TEACHERS</h1>
+      <h3 className="text-[16px] text-light-100 w-full max-w-170.75 text-center mb-20">
+        Hey choose our platform for high-quality teaching, flexible learning,
+        and professional support. From beginners to advanced learners, our
+        clients see real progress and lasting results.
+      </h3>
+      <div className="flex flex-col items-center gap-10 lg:flex-row lg:items-start">
+        content
+      </div>
+    </div>
+  );
+};
