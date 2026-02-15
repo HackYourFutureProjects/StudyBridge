b74e13665595730ab99065d96e25191899788ad3
Majoodeh <Majdhussein144@gmail.com>
2026-02-15 18:42:38 +0100

    refactor: Topbar responsive design

--- client/src/components/headerPrivate/TopBar.tsx
+++ client/src/components/headerPrivate/TopBar.tsx
@@ -8,15 +8,18 @@ export const TopBar = () => {
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   return (
     <>
-      <header className="fixed z-50 w-[1140px] h-[80px] top-[15px] left-[252px] pt-[16px] pr-[24px] pb-[16px] pl-[24px] gap-[32px] border-b border-[#0F0E13] flex flex-row items-center">
-        <div className="w-[350px] h-[48px] pt-[12px] pr-[18px] pb-[12px] pl-[18px] gap-[12px] rounded-[10px] border-[1.4px] border-[#E4E4E4] flex items-center">
-          <SearchIcon className="w-4 h-4 text-[#959595]" />
+      <header
+        className="fixed top-0 left-0 z-50 w-full h-[70px] px-4 flex flex-row items-center justify-between bg-[#211c27] border-b border-[#0F0E13] 
+md:left-[218px] md:w-[calc(100%-218px)] md:px-10"
+      >
+        <div className="flex-1 max-w-[200px] md:max-w-[350px] md:w-[350px] h-[48px] px-[18px] gap-[12px] rounded-[10px] border-[1.4px] border-[#E4E4E4] flex items-center">
+          <SearchIcon className="w-4 h-4 text-[#959595] shrink-0" />
 
           <input
             type="text"
             placeholder="Global Search"
             className="
-                    w-[278px] h-[19px]
+                    w-full h-full
                     text-[#F2F2F2] bg-transparent caret-[#F2F2F2]
                     placeholder:font-inter
                     placeholder:font-normal
@@ -28,22 +31,22 @@ export const TopBar = () => {
                     "
           />
         </div>
-
-        <div className="h-[38px] w-[710px] p-0 flex flex-row justify-end items-center gap-[32px] relative">
-          <button className="text-[#474747] hover:text-[#8A8A8A] transition-colors">
+        <div className="flex flex-row items-center gap-4 md:gap-[32px]">
+          <button className="cursor-pointer text-[#474747] hover:text-[#8A8A8A] transition-colors">
             <HelpIcon className="w-[19.5px] h-[19.5px]" />
           </button>
 
           {/* Vertical Divider */}
-          <div className="w-[1px] h-[33px] bg-[#E4E4E4]" />
+          <div className="w-px h-[33px] bg-[#E4E4E4]" />
 
           {/* Logout Button */}
           <button
             onClick={() => setIsLogoutModalOpen(true)}
-            className=" cursor-pointer flex items-center w-[92px] h-[22px] gap-5 group text-[#474747] hover:text-[#8A8A8A] transition-colors"
+            className=" cursor-pointer flex items-center gap-2 md:gap-5  text-[#474747] hover:text-[#8A8A8A] transition-colors"
           >
             <LogoutIcon className="w-[20px] h-[20px]" />
-            <span className="font-semibold text-[16px] leading-[100%] ">
+            <span className=" hidden font-semibold text-[16px] leading-[100%] md:block">
+              {/* On smaller screens, Logout word is hiiden */}
               Logout
             </span>
           </button>
