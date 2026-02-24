d31365817194e566a0c708f311c6ad23b2ff08d0
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2026-02-24 12:10:17 +0100

    fix: services types

--- server/src/services/student/student.service.ts
+++ server/src/services/student/student.service.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from "inversify";
 import { StudentCommand } from "../../repositories/commandRepositories/student.command.js";
 import { TYPES } from "../../composition/composition.types.js";
-import { StudentRegistrationType } from "../../types/student/student.types.js";
 import { StudentTypeDB } from "../../db/schemes/types/student.types.js";
 import bcrypt from "bcryptjs";
 import { randomUUID } from "node:crypto";
@@ -10,6 +9,7 @@ import { HttpError, NotFoundError } from "../../utils/error.util.js";
 import { StudentQuery } from "../../repositories/queryRepositories/student.query.js";
 import { TeacherQuery } from "../../repositories/queryRepositories/teacher.query.js";
 import { isMongoDuplicateKeyError } from "../../utils/duplicateType.guard.js";
+import { RegistrationType } from "../../types/auth/auth.types.js";
 @injectable()
 export class StudentService {
   constructor(
@@ -24,7 +24,7 @@ export class StudentService {
     lastName,
     password,
     role,
-  }: StudentRegistrationType) {
+  }: RegistrationType) {
     const studentExists = await this.studentQuery.getStudentByEmail(email);
     const teacherExists = await this.teacherQuery.getTeacherByEmail(email);
 
