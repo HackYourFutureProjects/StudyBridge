02402f1d7a7cb2ae4ab12a61cab7a3c539100d1b
Dmytro Doronin <138795636+Dmytro-Doronin@users.noreply.github.com>
2026-02-28 15:27:34 +0100

    fix: home filterr and textfields error paddings

--- client/src/components/ui/textField/TextField.tsx
+++ client/src/components/ui/textField/TextField.tsx
@@ -228,7 +228,9 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
             )}
           </div>
           {errorMessage && (
-            <span className="mt-1 text-sm text-danger">{errorMessage}</span>
+            <span className="mt-1 ml-6 text-sm text-danger">
+              {errorMessage}
+            </span>
           )}
         </div>
       </>
